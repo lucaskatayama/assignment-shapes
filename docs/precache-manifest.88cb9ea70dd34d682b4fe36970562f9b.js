d3546539392a58b9d40e84227cf37ff260b8ede4
Lucas Katayama <lucaskatayama@gmail.com>
2019-01-12 16:48:32 -0200

    WIP: docs

---
+++ docs/precache-manifest.88cb9ea70dd34d682b4fe36970562f9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "96375f91d56acc04b625",
+    "url": "/css/author.6381ac42.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "167a804ba19b88e50206",
+    "url": "/js/chunk-vendors.5e91cf09.js"
+  },
+  {
+    "revision": "96375f91d56acc04b625",
+    "url": "/js/author.d6a400ab.js"
+  },
+  {
+    "revision": "be3622a35c107ca3fdf6",
+    "url": "/js/application.4cd5484a.js"
+  },
+  {
+    "revision": "bbd86ac6a29ec33e81c8",
+    "url": "/js/app.79f1de29.js"
+  },
+  {
+    "revision": "46b85b6c4bccc0e69e72",
+    "url": "/js/about.4d4ca9a4.js"
+  },
+  {
+    "revision": "9870c639293328905a8298d98614323d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "167a804ba19b88e50206",
+    "url": "/css/chunk-vendors.061c6e71.css"
+  },
+  {
+    "revision": "be3622a35c107ca3fdf6",
+    "url": "/css/application.5da6d889.css"
+  },
+  {
+    "revision": "bbd86ac6a29ec33e81c8",
+    "url": "/css/app.4b59b2a8.css"
+  },
+  {
+    "revision": "46b85b6c4bccc0e69e72",
+    "url": "/css/about.dca5e9fd.css"
+  }
+];
